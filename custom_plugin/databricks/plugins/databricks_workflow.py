d00cac9149490ef7ce0159eb6afd3cbed9009339
Sunil Kunchoor <sunilkunchoor@gmail.com>
2025-12-06 11:11:38 +0000

    Modified the plugin name to avoid overlap

--- custom_plugin/databricks/plugins/databricks_workflow.py
+++ custom_plugin/databricks/plugins/databricks_workflow.py
@@ -3,7 +3,7 @@ from custom_plugin.databricks.hooks.databricks import DatabricksHook
 from custom_plugin.databricks.operators.databricks import DatabricksRunNowOperator, DatabricksJobRunLink
 
 class DatabricksWorkflowPlugin(AirflowPlugin):
-    name = "databricks_workflow"
+    name = "databricks_workflow_custom"
     hooks = [DatabricksHook]
     operators = [DatabricksRunNowOperator]
     operator_extra_links = [DatabricksJobRunLink()]
